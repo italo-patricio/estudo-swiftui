a0f111acbab9ba9d37dec3b499377ece550cae6b
Tech <tech@elo7.com>
2022-12-07 14:35:04 -0300

    Initial Commit

---
+++ profile-swiftui/profile_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_swiftuiApp.swift
+//  profile-swiftui
+//
+//  Created by Developer Elo7 on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct profile_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
